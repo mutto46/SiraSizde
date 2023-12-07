8f2c901dc2e4403ff98f9df45a0be68cccfad30e
muttalip591 <muttalip591@BPC13>
2023-12-07 13:37:02 +0300

    Proje dosyası ekle.

---
+++ SiraSizde1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiraSizde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ekranİslem ekranİslem = new Ekranİslem();
+            ekranİslem.EkranaYaz(0);
+            Console.WriteLine("=======================");
+            ekranİslem.EkranaYaz();
+        }
+        class Ekranİslem
+        {
+            public void EkranaYaz(params int[] sayilar)
+            {
+                if (sayilar.Length == 0)
+                {
+                    Console.WriteLine("Parametreden çıkılıyor");
+                    return;
+                }
+                Console.WriteLine("Parametreden gelen değer: ");
+                foreach (var s in sayilar)
+                {
+                    if (s == 0) return;
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
